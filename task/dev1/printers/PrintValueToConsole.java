c3797c4c564666704f54b032be9187f8ceaddacb
Ivan <ivanmikhailenka@gmail.com>
2017-02-08 16:59:34 +0200

    deleted one useless method

--- task/dev1/printers/PrintValueToConsole.java
+++ task/dev1/printers/PrintValueToConsole.java
@@ -8,20 +8,12 @@ import java.util.List;
 public class PrintValueToConsole {
 
   /**
-   * Print object to console
-   * @param value - input object witch need to print
-   */
-  private void printObject(Object value) {
-    System.out.println(value.toString());
-  }
-
-  /**
    * Print list to console
    * @param valueList - input list witch need to print
    */
   public void printList(List valueList) {
     for (Object value : valueList) {
-      printObject(value);
+      value.toString();
     }
   }
 }
